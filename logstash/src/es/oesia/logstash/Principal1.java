7825395efb5202fd4142808280940155b5c7f87a
cecilioAlvarez <cmac@arquitecturajava.com>
2023-10-26 09:51:56 +0200

    subemelo con threads

--- logstash/src/es/oesia/logstash/Principal1.java
+++ logstash/src/es/oesia/logstash/Principal1.java
@@ -7,14 +7,15 @@ import java.nio.file.StandardOpenOption;
 
 public class Principal1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		try {
 			String texto = "hola que tal estamos amigos \n";
 			for (int i = 0; i < 100; i++) {
 				Path fichero = Path.of(
 						"C:\\Users\\cecilioalvarezcaules\\Desktop\\oesialogtash\\logstash-8.10.4\\ficherosorigen\\fichero1.txt");
-				Files.writeString(fichero, texto,StandardOpenOption.APPEND);
+				Files.writeString(fichero, texto +i,StandardOpenOption.APPEND);
+				Thread.sleep(100);
 				String contenido = Files.readString(fichero);
 				System.out.println(contenido);
 			}
