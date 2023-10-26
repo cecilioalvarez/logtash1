c3b578b2c4b43e366c8154a121ce586183d88bc5
cecilioAlvarez <cmac@arquitecturajava.com>
2023-10-26 09:33:31 +0200

    ficheor generador lineas

--- logstash/src/es/oesia/logstash/Principal1.java
+++ logstash/src/es/oesia/logstash/Principal1.java
@@ -3,18 +3,21 @@ package es.oesia.logstash;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class Principal1 {
 
 	public static void main(String[] args) {
 
 		try {
-			String texto = "hola que tal estamos logstash";
-			Path fichero = Path
-					.of("C:\\Users\\cecilioalvarezcaules\\Desktop\\oesialogtash\\logstash-8.10.4\\ficherosorigen\\fichero1.txt");
-			Files.writeString(fichero, texto);
-			String contenido = Files.readString(fichero);
-			System.out.println(contenido);
+			String texto = "hola que tal estamos amigos \n";
+			for (int i = 0; i < 100; i++) {
+				Path fichero = Path.of(
+						"C:\\Users\\cecilioalvarezcaules\\Desktop\\oesialogtash\\logstash-8.10.4\\ficherosorigen\\fichero1.txt");
+				Files.writeString(fichero, texto,StandardOpenOption.APPEND);
+				String contenido = Files.readString(fichero);
+				System.out.println(contenido);
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
